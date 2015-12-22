74a43d958718f4de4ecd45cc17b43d5aa3dcf8a5
Martin Mauch <martin.mauch@gmail.com>
2015-12-22 12:33:50 +0100

    Make touchpad and touchscreen work

--- drivers/i2c/busses/i2c-designware-platdrv.c
+++ drivers/i2c/busses/i2c-designware-platdrv.c
@@ -106,7 +106,7 @@ static int dw_i2c_acpi_configure(struct platform_device *pdev)
 	dw_i2c_acpi_params(pdev, "SSCN", &dev->ss_hcnt, &dev->ss_lcnt, NULL);
 	dw_i2c_acpi_params(pdev, "FMCN", &dev->fs_hcnt, &dev->fs_lcnt,
 			   &dev->sda_hold_time);
-
+	dev->sda_hold_time = 30;
 	id = acpi_match_device(pdev->dev.driver->acpi_match_table, &pdev->dev);
 	if (id && id->driver_data)
 		dev->accessor_flags |= (u32)id->driver_data;
